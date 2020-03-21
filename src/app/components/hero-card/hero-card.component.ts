96ae3a93085d14cefdc9902e23ae35c3e78f8a9c
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2020-03-21 18:19:58 -0500

    finishing app heros

--- src/app/components/hero-card/hero-card.component.ts
+++ src/app/components/hero-card/hero-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,15 +10,21 @@ export class HeroCardComponent implements OnInit {
 
   @Input() heroe: any = {};
   @Input() idx: number;
+  // se le colcoa el nombre de lo que el padre escuchara
+  // Al event emitter se le coloca el dato que se trabajara
+  @Output() heroeSeleccionado: EventEmitter<number>
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    //Inicializando event emitter
+    this.heroeSeleccionado = new EventEmitter()
+  }
 
   ngOnInit() {
   }
 
   verHeroe() {
-    this.router.navigate(['/heroe', this.idx])
-
+    // this.router.navigate(['/heroe', this.idx])
+    this.heroeSeleccionado.emit(this.idx)
   }
 
 }
